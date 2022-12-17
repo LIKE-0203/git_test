98aa33e4727123a51112f52ced28cfed20bb5071
zhangzujun <1323379949@qq.com>
2022-12-17 14:41:27 +0800

    第二次提交

--- src/main/java/com/zzj/GitTestApplication.java
+++ src/main/java/com/zzj/GitTestApplication.java
@@ -8,7 +8,7 @@ public class GitTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
-        System.out.println("第一个版本的代码");
+        System.out.println("第二个版本的代码");
     }
 
 }
