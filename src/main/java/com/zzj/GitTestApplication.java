d130b4b021aa0621b9f02815494d867de0e3fc54
zhangzujun <1323379949@qq.com>
2022-12-17 14:54:03 +0800

    test分支的第一次提交

--- src/main/java/com/zzj/GitTestApplication.java
+++ src/main/java/com/zzj/GitTestApplication.java
@@ -9,6 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
         System.out.println("第三个版本的代码");
+        System.out.println("这是test分支的第一个版本的代码");
     }
 
 }
